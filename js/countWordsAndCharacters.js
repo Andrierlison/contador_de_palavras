75fe0e75279d467dc3d53263454ef10e47ca8ba6
Andrierlison <andrierlisonsilva8@gmail.com>
2021-02-15 09:43:04 +0000

    Formatted code

--- js/countWordsAndCharacters.js
+++ js/countWordsAndCharacters.js
@@ -1,10 +1,21 @@
 function countWordsAndCharacters() {
-  let textArea = document.getElementById("textArea").value
+  let textArea = document.getElementById("textArea").value;
 
-  let numberOfLetters = textArea.length
-  document.getElementById("numberOfLetters").innerHTML = "NUMBER OF CHARACTERS INSERTED " + numberOfLetters
+  let numberOfLetters;
+  let numberOfWords;
 
-  let numberOfWords = textArea.split(' ')
+  numberOfLetters = textArea.length;
+
+  if (textArea.includes("  ")) {
+    numberOfLetters = numberOfLetters - 1;
+  }
+
+  document.getElementById("numberOfLetters").innerHTML =
+    "NUMBER OF CHARACTERS INSERTED " + numberOfLetters;
+
+  numberOfWords = textArea.split(" ");
+
   for (i = 0; i < numberOfWords.length; i++);
-  document.getElementById('numberOfWords').innerHTML = "NUMBER OF WORDS OR SPACES " + i;
+  document.getElementById("numberOfWords").innerHTML =
+    "NUMBER OF WORDS OR SPACES " + i;
 }
